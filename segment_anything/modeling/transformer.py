585a99ce8583c5220260427a64b882c3790700c2
ZimingLi1204 <airbus3301204@gmail.com>
2023-05-19 10:48:40 +0800

    debug sam

--- segment_anything/modeling/transformer.py
+++ segment_anything/modeling/transformer.py
@@ -162,6 +162,7 @@ class TwoWayAttentionBlock(nn.Module):
 
         # Cross attention block, tokens attending to image embedding
         q = queries + query_pe
+
         k = keys + key_pe
         attn_out = self.cross_attn_token_to_image(q=q, k=k, v=keys)
         queries = queries + attn_out
