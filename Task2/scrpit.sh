5194ff40662319a4b785495045c3dbeb71558001
ZimingLi1204 <airbus3301204@gmail.com>
2023-05-22 11:57:41 +0800

    nothing

--- Task2/scrpit.sh
+++ Task2/scrpit.sh
@@ -1,6 +1,6 @@
-python main.py --group_name finetune --save_name bc5_weighit_decay0 \
---batch_size 5 --use_embedded --weight_decay 0 \
---gpu_id 3
+python main.py --group_name finetune --save_name bc5_weighit_decay1e-3_adam \
+--batch_size 5 --use_embedded --weight_decay 1.e-3 --optimizer Adam \
+--gpu_id 2
 
 python main.py --group_name finetune --save_name bc32_weighit_decay0 \
 --batch_size 32 --use_embedded --weight_decay 0 \
