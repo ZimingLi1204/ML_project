06740ff864795e11187930f2e704ac99e5c0da84
ZimingLi1204 <airbus3301204@gmail.com>
2023-06-05 10:57:23 +0800

    noting

--- Task2/scrpit.sh
+++ Task2/scrpit.sh
@@ -26,15 +26,15 @@ python main.py --group_name finetune_points --save_name bc32_wd1e-3_adamW_samlos
 --use_embedded --weight_list 1 1 --loss sam_loss --lr_schedular StepLR --step_size 2000 \
 --gpu_id 0
 
-python main.py --group_name finetune_grid_points --save_name bc16_wd1e-3_adamW_samloss_1:1_lrdecay_iouloss0.05_12points \
---batch_size 16  --weight_decay 1.e-3 --optimizer AdamW --promt_type grid_points \
+python main.py --group_name finetune_grid_points --save_name bc16_wd1e-4_adamW_samloss_1:1_lrdecay_iouloss0.05_16points \
+--batch_size 16  --weight_decay 1.e-4 --optimizer AdamW --promt_type grid_points \
 --use_embedded --weight_list 1 1 --loss sam_loss --lr_schedular StepLR --step_size 2000 \
---iou_scale 0.05 --point_num 12 \
+--iou_scale 0.05 --point_num 16 \
 --gpu_id 0
 
 
-python main.py --group_name finetune_grid_points --save_name bc16_wd1e-3_adamW_samloss_1:1_lrdecay_warmup_iouloss0.05_16points \
---batch_size 16  --weight_decay 1.e-3 --optimizer AdamW --promt_type grid_points \
+python main.py --group_name finetune_grid_points --save_name bc16_wd1e-4_adamW_samloss_1:1_lrdecay_warmup_iouloss0.05_16points \
+--batch_size 16  --weight_decay 1.e-4 --optimizer AdamW --promt_type grid_points \
 --use_embedded --weight_list 1 1 --loss sam_loss --lr_schedular StepLR --step_size 2000 --linear_warmup \
 --iou_scale 0.05 --point_num 16 \
 --gpu_id 0
@@ -58,5 +58,6 @@ single_point_center 68.41 log/finetune_single_point/bc32_wd1e-3_adamW_samloss_10
 single_point 63.08 log/finetune_single_point/bc32_wd1e-3_adamW_samloss_10:1_lrdecay_iouloss0.05/11.pth
 points 81.70 log/finetune_points/bc32_wd1e-3_adamW_samloss_1:1_lrdecay_iouloss0_8_8/19.pth
 points_center 78.6 log/finetune_points/bc32_wd1e-3_adamW_center_samloss_1:1_lrdecay_iouloss0_8_8/12.pth
-grid_points_16 80.03 log/finetune_grid_points/bc16_wd1e-3_adamW_samloss_1:1_lrdecay_iouloss0.05_16points/10.pth
+grid_points_16 80.18 log/finetune_grid_points/bc16_wd1e-4_adamW_samloss_1:1_lrdecay_iouloss0.05_16points/14.pth
 grid_points_24 83.45 log/finetune_grid_points/bc16_wd1e-3_adamW_samloss_1:1_lrdecay_warmup_iouloss0.05_24points/10.pth
+grid_points_12 73.26 log/finetune_grid_points/bc16_wd1e-3_adamW_samloss_1:1_lrdecay_iouloss0.05_12points/12.pth
